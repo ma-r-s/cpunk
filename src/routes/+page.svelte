99f5e3352aaa17f806a7659249439c4981e49b2f
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-04-09 16:27:44 -0500

    FC

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Body from '$lib/body.svelte';
+</script>
+
+<div class="h-dvh bg-white">
+	<div class="flex h-full">
+		<div class="basis-1/4 border"></div>
+		<div class="basis-1/12 border"></div>
+		<div class="basis-1/3 border">
+			<Body></Body>
+		</div>
+		<div class="basis-1/3 border"></div>
+	</div>
+</div>
